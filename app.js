4f885c26f516cf8a4a77c5a63b277406bce7f3eb
Tyler Olsen <tylerkolsen@gmail.com>
2024-08-13 13:59:10 -0600

    finished main lab and added hidden input for name

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import nunjucks from 'nunjucks';
+import sample from 'lodash.sample';
 
 const app = express();
 const port = '8000';
@@ -48,5 +49,33 @@ app.get('/hello', (req, res) => {
 // Handle the form from /hello and greet the user.
 app.get('/greet', (req, res) => {
   const name = req.query.name || 'stranger';
-  res.render('greet.html.njk', { name: name });
+  const compliment = sample(COMPLIMENTS);
+  res.render('greet.html.njk', { 
+    name: name,
+    compliment: compliment
+  });
 });
+
+// Handle the form from /greet
+app.get('/game', (req, res) => {
+  const gameInput = req.query.gameInput;
+  const hidName = req.query.name;
+  gameInput === 'yes' ? 
+  res.render('game.html.njk', {
+    hidName
+  })
+  : res.render('goodbye.html.njk', {
+    hidName
+  })
+})
+
+// Handle the form from game and generate the Madlib
+app.get('/madlib', (req, res) => {
+  const {person, color, noun, adj} = req.query
+  res.render('madlib.html.njk', {
+    person,
+    color,
+    noun,
+    adj
+  })
+})
